238b628755f002fed1cbe615ffb165e1ae417e5d
Fuad <sintfd@gmail.com>
2025-06-26 19:08:53 +0400

    start currency

---
+++ src/components/CurrencyRate.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+interface Props {
+  currency: string;
+}
+
+export default class CurrencyRate extends Component {
+  componentDidMount(): void {}
+
+  render() {
+    return <></>;
+  }
+}
